9003a347146d866f6c7cbfc70f5020d872bfb974
d.mikhaylov <d.mihaylov@alfacapital.ru>
2018-08-27 16:47:01 +0300

    String literal corrected

--- hello-world/src/app.java
+++ hello-world/src/app.java
@@ -14,7 +14,7 @@ public class app {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Success!);
+        System.out.println("Success!");
     }
     
 }
